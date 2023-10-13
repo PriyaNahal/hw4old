1327a638e7b68fdb0b8ef92720e576aab160e934
PriyaNahal <83884139+PriyaNahal@users.noreply.github.com>
2023-10-12 23:23:58 -0700

    Add files via upload

---
+++ bug1.py
@@ -0,0 +1,16 @@
+class Base:
+# TODO: there's code missing in one or more lines below
+class Circle():
+	def __init__(x, size):
+		super().__init__(x, y, size)
+
+	def draw(self):
+		return f"""
+({self.x}, {self.y})\n{self.size}
+
+def main():
+	c = Circle(1,2,3)
+	print(c.shape())
+	print(c.draw())
+
+main()
---
+++ ladder.py
@@ -0,0 +1,5 @@
+def my_steps(n):
+	if n >= 1 and n <= 25:
+		print(n)
+	else:
+		print('ValueErrorException')
